afbe8deaf6e4ec76f8a5eb646b51f49726d6ab85
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2026-06-19 10:54:03 +0700

    refactor: update Select2 version

--- webapps/berkas_digital_klaim/plafon_ranap.php
+++ webapps/berkas_digital_klaim/plafon_ranap.php
@@ -359,7 +359,7 @@ if($q_sp) $service_piutang = mysqli_fetch_assoc($q_sp);
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/js/select2.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
