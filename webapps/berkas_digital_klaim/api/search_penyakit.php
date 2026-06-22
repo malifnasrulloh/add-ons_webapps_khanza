0e88afa1d46915d0448c63b63a9e8f71d8734b7d
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2026-06-22 08:28:34 +0700

    fix: update require_once path for configuration file in search_penyakit.php

--- webapps/berkas_digital_klaim/api/search_penyakit.php
+++ webapps/berkas_digital_klaim/api/search_penyakit.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 ini_set('display_errors', 0);
-require_once('../../../conf/conf.php');
+require_once(__DIR__ . '/../../conf/conf.php');
 
 header('Content-Type: application/json');
 
